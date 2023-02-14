0bc3bfb4be1ec88b045505c219f00d8e7e12a9c4
Richard Riß <richard.riss@desy.de>
2023-02-14 12:36:15 +0100

    smoothed and filtered

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import os
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from matplotlib.widgets import Button
-from collections import OrderedDict
+from scipy.signal import savgol_filter
+import numpy as np
 import threading
 import pandas as pd
 
@@ -18,31 +19,13 @@ import zeroconf
 
 
 # ToDO: Export Timeframe of values
-# dev.write("DATA:ENCdg ASCII")
-#a = dev.query_ascii_values('CURV?')
-# b = [i/5.0 for i in a]
-'''
-dev.query("DATA:START?")
-'1'
-dev.query("DATA:STOP?")
-'10000'
-Anzahl return points
-
-dev.write('DATa:ENCdg RIBINARY') #--32768 to 32767
-dev.write('DATa:WIDth 2')
-dev.query_binary_values('CURVE?', datatype='h', is_big_endian=True)
-
-dev.query("WFMPre:XINcr?")
-'4.0E-7'
-Abtastrate
-'''
 
 
 class Storage:
     def __init__(self):
         self.col = ('time','values')
         self.values = pd.DataFrame(columns=self.col)
-        self.limit = 100000
+        self.limit_vals = 100000
         self.init_time = time.time()
         self.limit = True
         self._ch1_scale = None
@@ -80,13 +63,19 @@ class Storage:
     def write(self, tme: time.time, v:list):
         try:
             v = [((val * 10) / 65535) * self.ch1_scale for val in v]
+            #v = savgol_filter(v, 51, 3)
+            v = smooth_triangle(v, 10)
             t = [round(tme - (ts * self.incr) - self.init_time, 7) for ts in reversed(range(len(v)))]
             df = pd.DataFrame(data={self.col[0]:t, self.col[1]:v})
             df = df.query(f'{-self.graph_scale} < {self.col[1]} < {self.graph_scale}')
+            time_max = self.values[self.col[0]].max()
+            if time_max is np.nan: time_max = 0
+            df = df.query(f'{time_max} < {self.col[0]}')
             self.values = pd.concat([self.values, df], ignore_index=True)
             self.values.reset_index(drop=True)
             if self.limit:
-                self.values = self.values[-self.limit:]
+                self.values = self.values[-self.limit_vals:]
+
 
         except:
             logging.error(f'{sys.exc_info()[1]}')
@@ -106,9 +95,10 @@ class Device:
         # Init Device
         self.ch1_scale = ch1_scale
         self.incr = None
-        self.rm = pyvisa.ResourceManager('@py') #'@py'
+        self.rm = pyvisa.ResourceManager() #'@py'
         self.device = None
         self.graph = None
+        self._paused = False
         self.limit = True
         assert len(ip) > 0
         self.ip = ip
@@ -124,6 +114,13 @@ class Device:
                 logging.error(f'Error on line {sys.exc_info()[-1].tb_lineno}')
 
 
+    @property
+    def paused(self):
+        return self._paused
+
+    @paused.setter
+    def paused(self, value: bool):
+        self._paused = value
 
     def set_device(self, dev: pyvisa.resources.resource):
         try:
@@ -145,6 +142,7 @@ class Device:
             # set Mode to RIBinary (Width 2 = signed int)
             dev.write('DATa:ENCdg RIBINARY')
             dev.write('DATa:WIDth 2')
+            dev.write('HORizontal:RECOrdlength 10000')
 
         except pyvisa.errors.VisaIOError:
             logging.error(f'{sys.exc_info()[1]}')
@@ -153,6 +151,22 @@ class Device:
             raise pyvisa.errors.VisaIOError
 
 
+def pause(event, dev:Device):
+    dev.paused = not dev.paused
+
+
+def smooth_triangle(data, degree):
+    triangle=np.concatenate((np.arange(degree + 1), np.arange(degree)[::-1])) # up then down
+    smoothed=[]
+
+    for i in range(degree, len(data) - degree * 2):
+        point=data[i:i + len(triangle)] * triangle
+        smoothed.append(np.sum(point)/np.sum(triangle))
+    # Handle boundaries
+    smoothed=[smoothed[0]]*int(degree + degree/2) + smoothed
+    while len(smoothed) < len(data):
+        smoothed.append(smoothed[-1])
+    return smoothed
 
 
 def animate(frame:int, graph_max:int, ax:matplotlib.pyplot.Axes, storage: Storage):
@@ -176,17 +190,23 @@ def animate(frame:int, graph_max:int, ax:matplotlib.pyplot.Axes, storage: Storag
         logging.error(f'Error on line {sys.exc_info()[-1].tb_lineno}')
 
 
-def pull_data(device:pyvisa.resources.resource, storage: Storage):
+def pull_data(device:Device, storage: Storage):
     while True:
         try:
-            # Get Data
-            values = device.query_binary_values('CURVE?','h', True)
-            timestamp = time.time()
-            storage.write(timestamp, values)
+            if not device.paused:
+                # Get Data
+                values = device.device.query_binary_values('CURVE?','h', True)
+                timestamp = time.time()
+                storage.write(timestamp, values)
+
+            else:
+                time.sleep(0.5)
 
         except:
             logging.error(f'{sys.exc_info()[1]}')
             logging.error(f'Error on line {sys.exc_info()[-1].tb_lineno}')
+
+
 
 
 def init_logging():
@@ -218,7 +238,7 @@ def main():
     ip = "192.168.178.3"
     graph_max = 11.0
     ch1_scale = 5
-    interval = 100
+    interval = 10
     dev = Device(ip, ch1_scale)
 
     # Init figure
@@ -232,7 +252,7 @@ def main():
     storage.ch1_scale = ch1_scale
     storage.incr = dev.incr
     storage.graph_scale = graph_max
-    thr_data = threading.Thread(target = pull_data, args = (dev.device,storage), daemon = True)
+    thr_data = threading.Thread(target = pull_data, args = (dev,storage), daemon = True)
     thr_data.start()
 
     # Create Reset Button
@@ -244,6 +264,11 @@ def main():
     button_pos_limit = fig.add_axes([0.71, 0.9, 0.1, 0.075])
     button_limit = Button(button_pos_limit, 'Limit')
     button_limit.on_clicked(storage.limited)
+
+    # Create Pause Button
+    button_pos_pause = fig.add_axes([0.61, 0.9, 0.1, 0.075])
+    button_pause = Button(button_pos_pause, 'Pause')
+    button_pause.on_clicked(lambda x: pause(x,dev))
 
     ani = FuncAnimation(fig, animate, interval=interval, fargs=(graph_max,ax,storage), cache_frame_data=False)
     plt.show()
